c7dc23f25073e3f6658d81fb8b5801b92a3d644c
Munaf <191462807+munafmd9849@users.noreply.github.com>
2026-01-18 21:25:06 +0530

    Add Perfect Root [A] from Codeforces

---
+++ 2185/A - Perfect Root/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {	int t;	cin>>t;	while(t--){	    int n;	    cin>>n;	    for(int i=1;i<=n;i++){	        if(i==n)	            cout<<i<<"\n";	        else	            cout<<i<<" ";	    }	    	    	} }
