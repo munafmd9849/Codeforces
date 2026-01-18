3c6c43b8c7959382bb856dc9a7c3b1050ad494c4
Munaf <191462807+munafmd9849@users.noreply.github.com>
2026-01-18 20:58:35 +0530

    Add Prefix Max [B] from Codeforces

---
+++ 2185/B - Prefix Max/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {	int t;	cin>>t;	while(t--){	    int n;	    cin>>n;	    int arr[n];	    for(int i=0;i<n;i++){	        cin>>arr[i];	    }	    int maxi=arr[0];	    for(int i=0;i<n;i++){	        if(arr[i]>maxi){	            maxi=arr[i];	            	        }	    }	    cout<<maxi*n<<"\n";	    	} }
