bfed57b22626e6bdf07247575c22ea968e83a23e
Munaf <191462807+munafmd9849@users.noreply.github.com>
2026-02-08 13:31:42 +0530

    Add Line Trip [A] from Codeforces

---
+++ 1901/A - Line Trip/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {	int t;	cin>>t;	while(t--){	    int n;	    int x;	    cin>>n>>x;	    int arr[n];	    for(int i=0;i<n;i++){	        cin>>arr[i];	    }	    int diff=arr[0]-0;	    for(int i=1;i<n;i++){	        diff=max(diff,arr[i]-arr[i-1]);	    }	    	    cout<<max(2 * (x - arr[n-1]),diff)<<"\n";	} }
