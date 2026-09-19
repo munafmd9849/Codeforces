ba53c5ba7a66df9fad92b6e1ea4dd796132c0fdc
Munaf <191462807+munafmd9849@users.noreply.github.com>
2026-09-19 23:36:10 +0530

    Add Sieve of Erato67henes [A] from Codeforces

---
+++ 2195/A - Sieve of Erato67henes/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        bool flag=false;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            if(a[i]==67){
+                flag=true;
+            }
+        }
+        cout<<(flag?"Yes":"NO")<<"
+";
+    }
+ 
+ 
+}
